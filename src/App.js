ba788ecf9833bd64b78c8049e4ed2be0180c6ce9
Josh Smith <alotabits@bitsigil.io>
2022-01-25 07:41:04 -0500

    disable flaky trackers again

--- src/App.js
+++ src/App.js
@@ -291,9 +291,9 @@ export default function App() {
 
 			let announceURLs = [
 				"wss://tracker.openwebtorrent.com",
-				"wss://tracker.sloppyta.co:443/announce",
-				"wss://tracker.novage.com.ua:443/announce",
 				// Connections fail:
+				// "wss://tracker.sloppyta.co:443/announce",
+				// "wss://tracker.novage.com.ua:443/announce",
 				// "wss://tracker.btorrent.xyz:443/announce"
 			];
 
