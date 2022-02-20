b4e370dcc0253d04d38bd415ca6dff5770e85a50
Josh Smith <alotabits@bitsigil.io>
2022-02-19 19:42:11 -0500

    get all the logs

--- src/@log.js
+++ src/@log.js
@@ -4,9 +4,13 @@ const log = (...args) => {
 	queue.push(args);
 };
 
-console.log = log;
+console.log = (...args) => log("LOG", ...args);
+console.warn = (...args) => log("WARN", ...args);
+console.error = (...args) => log("ERROR", ...args);
 
 export const getLogQueue = (log) => {
-	console.log = log;
+	console.log = (...args) => log("LOG", ...args);
+	console.warn = (...args) => log("WARN", ...args);
+	console.error = (...args) => log("ERROR", ...args);
 	return queue;
 };
