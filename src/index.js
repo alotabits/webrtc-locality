7e6dd0bfa8f2933db754a320768c189f6740dee5
Josh Smith <alotabits@bitsigil.io>
2022-02-26 15:03:11 -0500

    bump version

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <StrictMode>
-    <App getLogQueue={getLogQueue} query={query} version="6" />
+    <App getLogQueue={getLogQueue} query={query} version="7" />
   </StrictMode>,
   rootElement
 );
